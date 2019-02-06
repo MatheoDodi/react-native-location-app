9f040a2086648f4683a39c3953cc02e656d963c5
MatthewDodi <matthew.dodi@gmail.com>
2019-02-06 15:59:47 -0800

    type: created new input element with TextInput component

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
+export default class App extends Component {
+  state = {
+    placeName: ''
+  };
 
-export default class App extends Component<Props> {
+  handleChangePlaceName = value => {
+    this.setState({ placeName: value });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          style={{ width: 300, borderColor: 'black', borderBottomWidth: 1 }}
+          value={this.state.placeName}
+          onChangeText={this.handleChangePlaceName}
+        />
       </View>
     );
   }
@@ -25,16 +28,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
   }
 });
