2214404b187008534478b512998e90dc5f1289d2
MatthewDodi <matthew.dodi@gmail.com>
2019-02-06 17:04:07 -0800

    type: feat - Added new handlePlaceSubmit that handles user inputs and displayes them in a list in the screen

--- App.js
+++ App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import { Row, SearchButton, Input } from './components/styles';
 
 export default class App extends Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   };
 
   handleChangePlaceName = value => {
     this.setState({ placeName: value });
   };
 
+  // Displays new input on button tap
+  handlePlaceSubmit = () => {
+    const { placeName, places } = this.state;
+    const newPlaces = [...places, placeName]; // add input to the places array
+
+    this.setState({ places: newPlaces, placeName: '' });
+  };
+
   render() {
+    const { placeName, places } = this.state;
+
     return (
       <View style={styles.container}>
         <Row>
           <Input
             style={{ borderColor: 'black', borderBottomWidth: 1 }}
-            value={this.state.placeName}
+            value={placeName}
             onChangeText={this.handleChangePlaceName}
             placeholder="Search for an awesome place"
           />
-          <SearchButton title="Add" />
+          <SearchButton onPress={this.handlePlaceSubmit} title="Add" />
         </Row>
+        {places.length > 0 &&
+          places.map(place => <Text key={place}>{place}</Text>)}
       </View>
     );
   }
