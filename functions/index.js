8a30728a648224957d83e125573f9b951698fa26
MatthewDodi <matthew.dodi@gmail.com>
2019-02-14 20:50:09 -0800

    type: feat additional configuration needed for firebase storage

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
 const fs = require('fs');
+const UUID = require('uuid-v4');
+
+const gc = {
+  projectId: 'react-hooks-matthew',
+  keyFilename: 'react-native-location.json'
+};
+const gcs = require('@google-cloud/storage')(gc);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,6 +18,17 @@ exports.storeImage = functions.https.onRequest((request, response) => {
     fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
       console.log(err);
       return response.status(500).json({ error: err });
+    });
+    const bucket = gcs.bucket('react-hooks-matthew.appspot.com');
+    const uuid = UUID();
+
+    bucket.upload('/tmp/uploaded-image.jpg', {
+      uploadType: 'media',
+      destination: '/places/' + uuid + '.jpg',
+      meta: {
+        contentType: 'images/jpeg',
+        firebaseStorageDownloadTokens: uuid
+      }
     });
   });
 
